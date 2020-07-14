4d624de4b206b961912fe1c4e4c09638881f0f35
shubhamkumar648 <55282520+shubhamkumar648@users.noreply.github.com>
2020-07-14 13:30:20 +0530

    Initial commit

---
+++ js/resp.js
@@ -0,0 +1,12 @@
+ burger = document.querySelector('.burger')
+ navbar =document.querySelector('.navbar')
+ navList =document.querySelector('.nav-list')
+ rightNav =document.querySelector('.right')
+
+
+ burger.addEventListener('click', ()=>{
+
+    navbar.classList.toggle('v-class-resp');
+    navbar.classList.toggle('v-class-resp');
+ 
+})
